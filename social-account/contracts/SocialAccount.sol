f3fd9b7a38f18c04c35e8bc88654d34b51a47098
Egor Suvorov <egor.suvorov@gmail.com>
2018-04-26 19:38:04 +0300

    SocialAccount: add name/owner

--- social-account/contracts/SocialAccount.sol
+++ social-account/contracts/SocialAccount.sol
@@ -1,4 +1,16 @@
 pragma solidity ^0.4.21;
 
 contract SocialAccount {
+    string public name;
+    address public owner;
+
+    constructor(string _name) public {
+        name = _name;
+        owner = msg.sender;
+    }
+
+    function setName(string _name) public {
+        require(msg.sender == owner);
+        name = _name;
+    }
 }
