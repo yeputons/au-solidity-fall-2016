9d24e260c04ff19cc79c5e7a9055d83ec4f9114f
Egor Suvorov <egor.suvorov@gmail.com>
2018-04-26 17:17:24 +0300

    Replace Migrations.sol with Chess.sol

---
+++ task-1-chess/contracts/Chess.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.0;
+
+contract Chess {
+    address public white;
+    address public black;
+    bool public whitesTurnNext;
+    bytes32[] public turns;
+    
+    constructor(address white_, address black_) public {
+        white = white_;
+        black = black_;
+        whitesTurnNext = true;
+    }
+    
+    function makeTurn(bytes32 turn) public {
+        if (whitesTurnNext) {
+            require(msg.sender == white);
+        } else {
+            require(msg.sender == black);
+        }
+        whitesTurnNext = !whitesTurnNext;
+        turns.push(turn);
+    }
+}
--- task-1-chess/migrations/1_initial_migration.js
+++ task-1-chess/migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var Chess = artifacts.require("./Chess.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Chess);
 };
