0552abbeb5c2cd8bff065df592bdc630be9d8b3f
Egor Suvorov <egor.suvorov@gmail.com>
2018-04-30 13:26:51 +0300

    SocialAccount: add addFriend, removeFriend, test that

--- social-account/contracts/SocialAccount.sol
+++ social-account/contracts/SocialAccount.sol
@@ -1,8 +1,16 @@
 pragma solidity ^0.4.21;
 
+import "./SocialConnection.sol";
+
 contract SocialAccount {
     string public name;
     address public owner;
+
+    // Stores following connections:
+    // 1. Initiated by me (owns such connections)
+    // 2. Accepted by me
+    // 3. Cancelled
+    mapping(address => SocialConnection) public connections;
 
     constructor(string _name) public {
         name = _name;
@@ -12,5 +20,53 @@ contract SocialAccount {
     function setName(string _name) public {
         require(msg.sender == owner);
         name = _name;
+    }
+
+    // `connection` is zero if we want to create a new `SocialConnection`
+    // and non-zero otherwise. This prevents frontrunning: user always knows
+    // what `SocialConnection` they accept and can check its code.
+    function addFriend(SocialAccount other, SocialConnection connection) public {
+        require(msg.sender == owner);
+        checkFriendRemoved(other);
+        require(connections[other] == SocialConnection(0));
+        if (connection == SocialConnection(0)) {
+            connections[other] = new SocialConnection(other);
+        } else {
+            // Ensure that the connection still exists and double-check.
+            require(connection.initiator() == other);
+            require(connection.acceptor() == this);
+            connections[other] = connection;
+            connection.accept();
+        }
+    }
+
+    // Unsafe as it doesn't check type of the proposed `SocialConnection`.
+    // It may be unknown in advance because of either malicious `SocialAccount`
+    // on the other end or frontrunning.
+    function addFriendWithAnyConnection(SocialAccount other) external {
+        addFriend(other, other.connections(this));
+    }
+
+    function checkFriendRemoved(SocialAccount other) public {
+        if (connections[other] != SocialConnection(0) && connections[other].status() == SocialConnection.Status.CANCELLED) {
+            delete connections[other];
+        }
+    }
+
+    function removeFriend(SocialAccount other) external {
+        require(msg.sender == owner);
+        SocialConnection conn = connections[other];
+        if (conn == SocialConnection(0)) {
+            return;
+        }
+        conn.cancel();  // We assume that SocialConnection is safe.
+        delete connections[other];
+
+        // Notify the other contract about friend removal, try to "safe push".
+        // This contract's `checkFriendRemoved`'s gas consumption is slightly
+        // below 8600, so there is some a margin.
+        if (!address(other).call.gas(10000)(bytes4(keccak256("checkFriendRemoved(address)")), this)) {
+          // It's more of a nice gesture than a necessity, so ignore all errors.
+        }
     }
 }
--- social-account/test/test_social_account.js
+++ social-account/test/test_social_account.js
@@ -1,4 +1,14 @@
 var SocialAccount = artifacts.require("SocialAccount");
+var SocialConnection = artifacts.require("SocialConnection");
+
+const PROPOSED = 0;
+const ACCEPTED = 1;
+const CANCELLED = 2;
+
+const ZERO_ADDRESS = "0x" + "0".repeat(40);
+
+const ACCEPTOR = "acceptor";
+const INITIATOR = "initiator";
 
 contract("SocialAccount", async (accounts) => {
   it("should initialize correctly", async () => {
@@ -16,6 +26,53 @@ contract("SocialAccount", async (accounts) => {
   it("prohibits name changes by others", async () => {
     let acc = await SocialAccount.new("My Name");
     await expectThrow(acc.setName("Other Name", {from: accounts[1]}));
+  });
+
+  it("befriends other accounts", async() => {
+    let acc1 = await SocialAccount.new("Name1");
+    let acc2 = await SocialAccount.new("Name2");
+    await acc1.addFriendWithAnyConnection(acc2.address);
+
+    let conn = SocialConnection.at(await acc1.connections(acc2.address));
+    assert.equal(await conn.initiator(), acc1.address);
+    assert.equal(await conn.acceptor(), acc2.address);
+    assert.equal(await conn.status(), PROPOSED);
+
+    await acc2.addFriendWithAnyConnection(acc1.address);
+    assert.equal(await conn.status(), ACCEPTED);
+    assert.equal(await acc2.connections(acc1.address), conn.address);
+  });
+
+  [ACCEPTOR, INITIATOR].forEach(param => {
+    it("befriends after acceptance and " + param + " cancels", async () => {
+      let acc1 = await SocialAccount.new("Name1");
+      let acc2 = await SocialAccount.new("Name2");
+      await acc1.addFriendWithAnyConnection(acc2.address);
+      let conn = SocialConnection.at(await acc1.connections(acc2.address));
+      await acc2.addFriendWithAnyConnection(acc1.address);
+
+      if (param == INITIATOR) {
+        await acc1.removeFriend(acc2.address);
+      } else if (param == ACCEPTOR) {
+        await acc2.removeFriend(acc1.address);
+      } else {
+        assert.isTrue(false);
+      }
+
+      assert.equal(await conn.status(), CANCELLED);
+      assert.equal(await acc1.connections(acc2.address), ZERO_ADDRESS);
+      assert.equal(await acc2.connections(acc1.address), ZERO_ADDRESS);
+
+      await acc1.addFriendWithAnyConnection(acc2.address);
+      let conn2 = SocialConnection.at(await acc1.connections(acc2.address));
+      assert.equal(await conn2.status(), PROPOSED);
+
+      await acc2.addFriendWithAnyConnection(acc1.address);
+      assert.equal(await conn2.status(), ACCEPTED);
+      assert.equal(await acc1.connections(acc2.address), conn2.address);
+      assert.equal(await acc2.connections(acc1.address), conn2.address);
+      assert.equal(await conn.status(), CANCELLED);
+    });
   });
 });
 
