b47e25f4e9739f9665e80c003d49c56de0386f7e
Egor Suvorov <egor.suvorov@gmail.com>
2018-04-30 17:50:30 +0300

    SocialConnection: add and emit StatusUpdated() event

--- social-account/contracts/SocialConnection.sol
+++ social-account/contracts/SocialConnection.sol
@@ -5,6 +5,8 @@ import "./SocialAccount.sol";
 contract SocialConnection {
     enum Status { PROPOSED, ACCEPTED, CANCELLED }
 
+    event StatusUpdated(SocialConnection indexed connection, SocialAccount indexed from, SocialAccount indexed to, Status oldStatus, Status newStatus);
+
     SocialAccount public initiator;
     SocialAccount public acceptor;
     Status public status;
@@ -14,12 +16,13 @@ contract SocialConnection {
         initiator = SocialAccount(msg.sender);
         acceptor = _acceptor;
         status = Status.PROPOSED;
+        emit StatusUpdated(this, initiator, acceptor, status, status);
     }
 
     function accept() public {
         require(msg.sender == address(acceptor));
         require(status <= Status.ACCEPTED);
-        status = Status.ACCEPTED;
+        changeStatus(Status.ACCEPTED);
     }
 
     function cancel() public {
@@ -27,6 +30,14 @@ contract SocialConnection {
         // Do not notify either contract because external call may fail, thus
         // blocking friendship cancellation, which is no good.
         // "Favor pull over push".
-        status = Status.CANCELLED;
+        changeStatus(Status.CANCELLED);
+    }
+
+    function changeStatus(Status newStatus) private {
+        if (status == newStatus) {
+            return;
+        }
+        emit StatusUpdated(this, initiator, acceptor, status, newStatus);
+        status = newStatus;
     }
 }
