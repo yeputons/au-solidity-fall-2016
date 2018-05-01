954b315a195ed8cbe3443f3df1fd6c1af521b266
Egor Suvorov <egor.suvorov@gmail.com>
2018-05-01 16:37:13 +0300

    Moycoin: test transferFrom and allowances (happy path)

--- erc-20-token/test/test_erc20.js
+++ erc-20-token/test/test_erc20.js
@@ -69,6 +69,66 @@ contract("Moycoin", async (accounts) => {
     await coin.transfer(accounts[0], 10, {from: accounts[1]});
     await coin.transfer(accounts[1], 123456);
   });
+
+  it("starts with zero allowances", async () => {
+    assert.equal(await coin.allowance.call(accounts[0], accounts[0]), 0);
+    assert.equal(await coin.allowance.call(accounts[0], accounts[1]), 0);
+    assert.equal(await coin.allowance.call(accounts[1], accounts[1]), 0);
+    assert.equal(await coin.allowance.call(accounts[1], accounts[2]), 0);
+  });
+
+  it("overrides allowance", async () => {
+    let res1 = await coin.approve(accounts[1], 100);
+    let res2 = await coin.approve(accounts[2], 10, {from: accounts[1]});
+    let res3 = await coin.approve(accounts[1], 50);
+    assert.equal(await coin.allowance.call(accounts[0], accounts[0]), 0);
+    assert.equal(await coin.allowance.call(accounts[0], accounts[1]), 50);
+    assert.equal(await coin.allowance.call(accounts[1], accounts[0]), 0);
+    assert.equal(await coin.allowance.call(accounts[1], accounts[1]), 0);
+    assert.equal(await coin.allowance.call(accounts[1], accounts[2]), 10);
+    assert.equal(await coin.allowance.call(accounts[2], accounts[1]), 0);
+
+    assert.equal(res1.logs.length, 1);
+    assertEqualEvent(res1.logs[0], {
+      event: "Approval",
+      args: {owner: accounts[0], spender: accounts[1], value: 100},
+    });
+
+    assert.equal(res2.logs.length, 1);
+    assertEqualEvent(res2.logs[0], {
+      event: "Approval",
+      args: {owner: accounts[1], spender: accounts[2], value: 10},
+    });
+
+    assert.equal(res3.logs.length, 1);
+    assertEqualEvent(res3.logs[0], {
+      event: "Approval",
+      args: {owner: accounts[0], spender: accounts[1], value: 50},
+    });
+  });
+
+  it("allows transferFrom", async () => {
+    await coin.approve(accounts[1], 100);
+    let res1 = await coin.transferFrom(accounts[0], accounts[2], 30, {from: accounts[1]});
+    let res2 = await coin.transferFrom(accounts[0], accounts[3], 60, {from: accounts[1]});
+    assert.equal(await coin.balanceOf.call(accounts[0]), 123366);
+    assert.equal(await coin.balanceOf.call(accounts[1]), 0);
+    assert.equal(await coin.balanceOf.call(accounts[2]), 30);
+    assert.equal(await coin.balanceOf.call(accounts[3]), 60);
+    assert.equal(await coin.allowance.call(accounts[0], accounts[1]), 10);
+
+    assert.equal(res1.logs.length, 1);
+    assertEqualEvent(res1.logs[0], {
+      event: "Transfer",
+      args: {from: accounts[0], to: accounts[2], value: 30}
+    });
+
+    assert.equal(res2.logs.length, 1);
+    assertEqualEvent(res2.logs[0], {
+      event: "Transfer",
+      args: {from: accounts[0], to: accounts[3], value: 60}
+    });
+  });
 });
 
 const assertEqualEvent = function(haystack, needle) {
