07aab8bb15d007e12c303335d80e1544d0f8cab1
Egor Suvorov <egor.suvorov@gmail.com>
2018-05-01 13:24:57 +0300

    Moycoin: replace implicit getters with explicit implementations of interface; now it deploys

--- erc-20-token/contracts/Moycoin.sol
+++ erc-20-token/contracts/Moycoin.sol
@@ -2,30 +2,38 @@ pragma solidity ^0.4.21;
 
 import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 
-contract Moycoin is DetailedERC20("Moycoin", "MOY", 0) {
-    uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
+contract Moycoin is DetailedERC20("Moycoin", "MOY", 1) {
+    uint256 public initialSupply;
+    mapping(address => uint256) public balances;
     mapping(address => mapping(address => uint256)) internal allowances;
 
-    constructor(uint256 _totalSupply) public {
-        totalSupply = _totalSupply;
-        balanceOf[msg.sender] = totalSupply;
+    constructor(uint256 _initialSupply) public {
+        initialSupply = _initialSupply;
+        balances[msg.sender] = initialSupply;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return initialSupply;
+    }
+
+    function balanceOf(address who) public view returns (uint256) {
+        return balances[who];
     }
 
     function transfer(address to, uint256 value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= value);
-        balanceOf[msg.sender] -= value;
-        balanceOf[to] += value;  // No overflow as totalSupply fits in uint256
+        require(balances[msg.sender] >= value);
+        balances[msg.sender] -= value;
+        balances[to] += value;  // No overflow as totalSupply fits in uint256
         emit Transfer(msg.sender, to, value);
         return true;
     }
 
     function transferFrom(address from, address to, uint256 value) public returns (bool success) {
         require(allowances[from][msg.sender] >= value);
-        require(balanceOf[from] >= value);
+        require(balances[from] >= value);
         allowances[msg.sender][from] -= value;
-        balanceOf[from] -= value;
-        balanceOf[to] += value;  // No overflow as totalSupply fits in uint256
+        balances[from] -= value;
+        balances[to] += value;  // No overflow as totalSupply fits in uint256
         emit Transfer(from, to, value);
         return true;
     }
