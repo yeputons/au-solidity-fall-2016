51f5613dd00b4ce111ed4861d4706973b74a1140
Egor Suvorov <egor.suvorov@gmail.com>
2018-05-01 13:42:54 +0300

    Moycoin: simplify test code

--- erc-20-token/test/test_erc20.js
+++ erc-20-token/test/test_erc20.js
@@ -1,30 +1,32 @@
 const Moycoin = artifacts.require("Moycoin");
 
 contract("Moycoin", async (accounts) => {
+  let coin;
+
   before(async () => {
-    this.coin = await Moycoin.new(123456, {gasLimit: 10000000});
+    coin = await Moycoin.new(123456);
   });
 
   it("returns correct name", async () => {
-    assert.equal(await this.coin.name.call(), "Moycoin");
+    assert.equal(await coin.name.call(), "Moycoin");
   });
 
   it("returns correct symbol", async () => {
-    assert.equal(await this.coin.symbol.call(), "MOY");
+    assert.equal(await coin.symbol.call(), "MOY");
   });
 
   it("returns correct decimals", async () => {
-    assert.equal(await this.coin.decimals.call(), 1);
+    assert.equal(await coin.decimals.call(), 1);
   });
 
   it("returns correct totalSupply", async () => {
-    assert.equal(await this.coin.totalSupply.call(), 123456);
+    assert.equal(await coin.totalSupply.call(), 123456);
   });
 
   it("provides initial balance for the creator only", async () => {
-    assert.equal(await this.coin.balanceOf.call(accounts[0]), 123456);
-    assert.equal(await this.coin.balanceOf.call(accounts[1]), 0);
-    assert.equal(await this.coin.balanceOf.call(accounts[2]), 0);
-    assert.equal(await this.coin.balanceOf.call(0), 0);
+    assert.equal(await coin.balanceOf.call(accounts[0]), 123456);
+    assert.equal(await coin.balanceOf.call(accounts[1]), 0);
+    assert.equal(await coin.balanceOf.call(accounts[2]), 0);
+    assert.equal(await coin.balanceOf.call(0), 0);
   });
 });
