1a8811660f66a437216e1eb2e8857de518f9003c
Egor Suvorov <egor.suvorov@gmail.com>
2018-05-01 13:37:38 +0300

    Moycoin: test initial balances

--- erc-20-token/test/test_erc20.js
+++ erc-20-token/test/test_erc20.js
@@ -1,6 +1,6 @@
 const Moycoin = artifacts.require("Moycoin");
 
-contract("Moycoin", async () => {
+contract("Moycoin", async (accounts) => {
   before(async () => {
     this.coin = await Moycoin.new(123456, {gasLimit: 10000000});
   });
@@ -19,5 +19,12 @@ contract("Moycoin", async () => {
 
   it("returns correct totalSupply", async () => {
     assert.equal(await this.coin.totalSupply.call(), 123456);
+  });
+
+  it("provides initial balance for the creator only", async () => {
+    assert.equal(await this.coin.balanceOf.call(accounts[0]), 123456);
+    assert.equal(await this.coin.balanceOf.call(accounts[1]), 0);
+    assert.equal(await this.coin.balanceOf.call(accounts[2]), 0);
+    assert.equal(await this.coin.balanceOf.call(0), 0);
   });
 });
